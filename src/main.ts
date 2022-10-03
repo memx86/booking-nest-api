ae0067c6bcdde2d3a19884a9bac1567c9295c1bf
memx86 <memx86@gmail.com>
2022-10-03 23:32:08 +0300

    PORT config variable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
